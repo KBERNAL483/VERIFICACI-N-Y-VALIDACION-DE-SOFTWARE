3b9db739dbb04d09164ca0ffcce2f591496c05a7
KBERNAL483 <kalebernal.889@gmail.com>
2024-10-12 13:28:10 -0600

    Tarea VYV

---
+++ Puntodeventa/src/main/java/com/mycompany/puntodeventa/PuntoDeVenta.java
@@ -0,0 +1,58 @@
+
+package com.mycompany.puntodeventa;
+
+import java.util.List;
+import mx.com.vafis.puntodeventa.database.ConexionSQLite;
+import mx.com.vafis.puntodeventa.database.UsuarioDAO;
+
+
+public class PuntoDeVenta {
+
+    public static void main(String[] args) {
+        ConexionSQLite.conectar();
+        
+        UsuarioDAO usuarioDao = new UsuarioDAO();
+        
+        List<Usuario> usuarios;
+        usuarios = usuarioDao.leerTodosLosUsuarios();
+        imprimirUsuarios(usuarios);
+        
+        usuarioDao.crearUsuario(new Usuario(0, "Jose", "jose@mail.com", "12345", "admin"));
+        usuarioDao.crearUsuario(new Usuario(0, "Alicia", "alicia@mail.com", "12345", "vendedor"));
+        usuarioDao.crearUsuario(new Usuario(0, "Juan", "juan@mail.com", "12345", "inventarios"));
+        
+        
+        usuarios = usuarioDao.leerTodosLosUsuarios();
+        imprimirUsuarios(usuarios);
+        
+        Usuario pedro = usuarioDao.leerUsuarioPorCorreo("pedro@mail.com");
+        
+        if(pedro != null){
+            pedro.setNombre("Pedro Valenzuela");
+            
+            usuarioDao.actualizarUsuario(pedro);
+            usuarios = usuarioDao.leerTodosLosUsuarios();
+            imprimirUsuarios(usuarios);
+            
+            usuarioDao.eliminarUsuario(pedro.getId());            
+            usuarios = usuarioDao.leerTodosLosUsuarios();
+            imprimirUsuarios(usuarios);
+        }
+        
+        
+        ConexionSQLite.cerrarConexion();
+    }
+    
+    public static void imprimirUsuarios(List<Usuario> usuarios){
+        
+        if(!usuarios.isEmpty()){
+            System.out.println("=============== USUARIOS ===============");
+        
+            for(Usuario usuario : usuarios){
+                System.out.println(usuario);
+            }
+        }else{
+            System.err.println("No hay usuarios registrados");
+        }
+    }
+}
